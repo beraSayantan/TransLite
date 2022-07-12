a23a2f74dde35374533f01170d348153ca9a755f
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-07-12 19:51:42 +0530

    "Multiples clickes required" problem solved
    Put in helper functions inside each onSuccessListener

--- app/src/main/java/com/beginner/translite/MainActivity.kt
+++ app/src/main/java/com/beginner/translite/MainActivity.kt
@@ -44,65 +44,59 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
         translatebutton.setOnClickListener {
 
-//            val languageIdentifier = LanguageIdentification.getClient()
-//            languageIdentifier.identifyLanguage(inputtext.text.toString())
-//                .addOnSuccessListener { languageCode ->
-//                    lcode = languageCode
-//                    if (languageCode == "und") {
-//                        Toast.makeText(applicationContext, "Can't identify language", Toast.LENGTH_SHORT).show()
-//                    } else {
-//                        Toast.makeText(applicationContext, lcode, Toast.LENGTH_SHORT).show()
-//                    }
-//                }
-//                .addOnFailureListener {
-//                    // Model couldn’t be loaded or other internal error.
-//                    // ...
-//                }
-
-            val options = TranslatorOptions.Builder()
-                //.setSourceLanguage(TranslateLanguage.fromLanguageTag(lcode)!!)
-                .setSourceLanguage(TranslateLanguage.ENGLISH)
-                .setTargetLanguage(TranslateLanguage.)
-                .build()
-
-            val translator = Translation.getClient(options)
-            getLifecycle().addObserver(translator)
-
-            //showProgressDialog("Loading, please wait for a few millenia")
-            val conditions = DownloadConditions.Builder().build()
-
-            showProgressDialog("Loading please wait\nIt may take a minute or two")
-            translator.downloadModelIfNeeded(conditions)
-                .addOnSuccessListener {
-
-                    translator.translate(inputtext.text.toString())
-                        .addOnSuccessListener { translatedText ->
-                            translatedheading.text = translatedText
-                        }
-                        .addOnFailureListener { exception ->
-                        }
-
-                    hideProgressDialog()
-
+            val languageIdentifier = LanguageIdentification.getClient()
+            languageIdentifier.identifyLanguage(inputtext.text.toString())
+                .addOnSuccessListener { languageCode ->
+                    lcode = languageCode
+                    if (languageCode == "und") {
+                        Toast.makeText(applicationContext, "Can't identify language", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(applicationContext, lcode, Toast.LENGTH_SHORT).show()
+                        TakesLcodePutsTheTranslationInTheTV(lcode, inputtext, translatedheading)
+                    }
                 }
-                .addOnFailureListener { exception ->
-                    // Model couldn’t be downloaded or other internal error.
+                .addOnFailureListener {
+                    // Model couldn’t be loaded or other internal error.
                     // ...
                 }
-//            translator.translate(inputtext.text.toString())
-//                .addOnSuccessListener { translatedText ->
-//                    translatedheading.text = translatedText
-//                }
-//                .addOnFailureListener { exception ->
-//                    Toast.makeText(this,exception.toString(),Toast.LENGTH_SHORT).show()
 //
-//                }
 
         }
 
 
+    }
+
+    private fun TakesLcodePutsTheTranslationInTheTV(
+        lcode: String,
+        inputtext: TextInputEditText,
+        translatedheading: TextView
+    ) {
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.fromLanguageTag(lcode)!!)
+            .setTargetLanguage(TranslateLanguage.FINNISH)
+            .build()
+
+        val translator = Translation.getClient(options)
+        getLifecycle().addObserver(translator)
+
+        //showProgressDialog("Loading, please wait for a few millenia")
+        val conditions = DownloadConditions.Builder().build()
+
+        showProgressDialog("Loading please wait\nIt may take a minute or two")
+        translator.downloadModelIfNeeded(conditions)
+            .addOnSuccessListener {
+
+                translateFromModelAndPutInTheTextView(translator, inputtext, translatedheading)
+                hideProgressDialog()
+
+            }
+            .addOnFailureListener { exception ->
+                // Model couldn’t be downloaded or other internal error.
+                // ...
+            }
     }
 
 
@@ -115,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         mProgressDialog.dismiss()
     }
 
-    fun translateFromModel(translator: Translator, inputtext:TextView, translatedheading:TextView){
+    fun translateFromModelAndPutInTheTextView(translator: Translator, inputtext:TextView, translatedheading:TextView){
         translator.translate(inputtext.text.toString())
             .addOnSuccessListener { translatedText ->
                 translatedheading.text = translatedText
