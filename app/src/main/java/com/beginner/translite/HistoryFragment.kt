e582a15ae870e5ad453671e23e9288a6a2c62708
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-07-23 10:15:15 +0530

    2 more fragments added, navigation graph added

--- app/src/main/java/com/beginner/translite/HistoryFragment.kt
+++ app/src/main/java/com/beginner/translite/HistoryFragment.kt
@@ -7,19 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.beginner.translite.databinding.FragmentHistoryBinding
-import com.beginner.translite.databinding.FragmentTransBinding
 
-class TransFragment : Fragment() {
+class HistoryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Inflate the layout for this fragment
 
-        val binding = DataBindingUtil.inflate<FragmentTransBinding>(inflater
-            ,R.layout.fragment_trans,container,false)
+        val binding = DataBindingUtil.inflate<FragmentHistoryBinding>(inflater
+            ,R.layout.fragment_history,container,false)
         return binding.root
     }
-
 
 }
