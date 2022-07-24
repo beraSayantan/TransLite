077ca1dca4d5329fdd9f3324efb5174e24328bda
shashankRanaNoidaWala <101785829+shashankRanaNoidaWala@users.noreply.github.com>
2022-07-24 10:52:23 +0530

    Implemented floating button

--- app/src/main/java/com/beginner/translite/AboutFragment.kt
+++ app/src/main/java/com/beginner/translite/AboutFragment.kt
@@ -1,20 +1,48 @@
 package com.beginner.translite
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
+import com.beginner.translite.databinding.FragmentAboutBinding
+import com.beginner.translite.databinding.FragmentTransBinding
 
 class AboutFragment : Fragment() {
+
+    private lateinit var currContext: Context
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+//        val shashankCard = view?.findViewById<CardView>(R.id.cvShashank)
+        var binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater
+            ,R.layout.fragment_about,container,false)
+
+        binding.cvShashank.setOnClickListener{
+            Toast.makeText(currContext,"Share",Toast.LENGTH_LONG).show()
+        }
+
+        binding.civShashank.setOnClickListener {
+            Toast.makeText(currContext,"Share",Toast.LENGTH_LONG).show()
+        }
+
         (activity as AppCompatActivity).supportActionBar?.title = "About Us"
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
+
+    override fun onAttach(context: Context) {
+
+        currContext=context
+        super.onAttach(context)
+    }
+
 }
